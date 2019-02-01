5a97d871396afaf3d507b26174f76e47dbcf9fe3
kolosinp <43353093+kolosinp@users.noreply.github.com>
2019-02-01 21:18:39 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,5 +53,5 @@ git pull
 
 # run provisioning
 ansible-playbook playbooks/apache.yml -u $(whoami) --ask-become-pass
-#ansible-playbook playbooks/php.yml
+ansible-playbook playbooks/php.yml
 ansible-playbook main.yml --ask-become-pass
